c47fc98fd246c89572cac4813b987896eec442a7
achtuber <achtuber@gmail.com>
2022-03-19 16:49:15 +0530

    fedfvdf

--- db.js
+++ db.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://haydiiusr:Hyd1105pwd@15.184.202.217:27017/haydiidb', {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
-});
-
-
-// mongoose.connect('mongodb://localhost:27017/haydiidb', {
+// mongoose.connect('mongodb://haydiiusr:Hyd1105pwd@15.184.202.217:27017/haydiidb', {
 //   useNewUrlParser: true,
 //   useFindAndModify: false,
 //   useUnifiedTopology: true
 // });
+
+
+mongoose.connect('mongodb://localhost:27017/haydiidb', {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
