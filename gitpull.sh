2607e841c91a4871c5aa97027d22f51580c2e618
achtuber <achtuber@gmail.com>
2022-03-19 17:38:28 +0530

    fedfvdf

--- gitpull.sh
+++ gitpull.sh
@@ -1,7 +1,3 @@
 while true ; do
-      if [[ -f GITPULLMASTER ]] ; then
-            git pull > gitpull.log 2>&1
-            mv GITPULLMASTER GITPULLMASTER.`date +"%Y%m%d%H%M%S"`
-      fi
-      sleep 10
+      gitpull
 done
